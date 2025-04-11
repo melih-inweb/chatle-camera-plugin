035b32e42f9b9f052fda784ab4ab6ad2baef0a12
dhruvkathiriya <dhruvkathiriya.retrytech@gmail.com>
2025-04-11 09:03:35 +0530

    - Add share to instagram

--- android/src/main/kotlin/com/retrytech/retrytech_plugin/RetrytechPlugin.kt
+++ android/src/main/kotlin/com/retrytech/retrytech_plugin/RetrytechPlugin.kt
@@ -1,22 +1,27 @@
 package com.retrytech.retrytech_plugin
 
+import android.app.Activity
+import android.content.Intent
 import android.util.Log
+import androidx.core.content.ContextCompat.startActivity
 import com.arthenica.ffmpegkit.FFmpegKit
 import com.arthenica.ffmpegkit.SessionState
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** RetrytechPlugin */
-class RetrytechPlugin : FlutterPlugin, MethodCallHandler {
+class RetrytechPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
     /// The MethodChannel that will the communication between Flutter and native Android
     ///
     /// This local reference serves to register the plugin with the Flutter Engine and unregister it
     /// when the Flutter Engine is detached from the Activity
     private lateinit var channel: MethodChannel
-
+    var context: Activity? = null
     override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         channel = MethodChannel(
             flutterPluginBinding.binaryMessenger,
@@ -36,6 +41,11 @@ class RetrytechPlugin : FlutterPlugin, MethodCallHandler {
                 }
             }
 
+            "shareToInstagram" -> {
+                Log.d("TAG", "onMethodCall: ${call.arguments}")
+                shareToInstagram(call.arguments.toString(), result)
+            }
+
             else -> {
                 result.notImplemented()
             }
@@ -44,5 +54,39 @@ class RetrytechPlugin : FlutterPlugin, MethodCallHandler {
 
     override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
         channel.setMethodCallHandler(null)
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        context = binding.activity
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    }
+
+    override fun onDetachedFromActivity() {
+    }
+
+    fun shareToInstagram(url: String, result: Result) {
+
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.type = "text/plain"
+        shareIntent.putExtra(Intent.EXTRA_TEXT, url)
+        val pm = context!!.packageManager
+
+        pm.queryIntentActivities(shareIntent, 0)
+        shareIntent.setPackage("com.instagram.android")
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT)
+        context!!.startActivity(shareIntent)
+
+        try {
+            Log.d("TAG", "onMethodCall: Sucess $shareIntent")
+            result.success(true)
+        } catch (e: Exception) {
+            Log.e("TAG", "onMethodCall: Failed $shareIntent $e")
+            result.success(false)
+        }
     }
 }
--- lib/retrytech_plugin.dart
+++ lib/retrytech_plugin.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/services.dart';
 
-import 'retrytech_plugin_platform_interface.dart';
-
 class RetrytechPlugin {
   final methodChannel = const MethodChannel('retrytech_plugin');
 
   Future<bool?> runTheCommand(String command) {
     return methodChannel.invokeMethod("runFFmpegCommand", command);
+  }
+
+  Future<bool?> shareToInstagram(String command) {
+    return methodChannel.invokeMethod("shareToInstagram", command);
   }
 }
