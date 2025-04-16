e2daf7e53fffa883186f4f8dac89215da38496d3
Jeel Khokhariya <jeelkhokhariya59@gmail.com>
2025-04-16 09:45:57 +0530

    remove ffmpeg

--- lib/retrytech_plugin.dart
+++ lib/retrytech_plugin.dart
@@ -10,4 +10,38 @@ class RetrytechPlugin {
   Future<bool?> shareToInstagram(String command) {
     return methodChannel.invokeMethod("shareToInstagram", command);
   }
+
+  Future<bool?> mergeAudioAndVideo(
+      {required String inputPath, required String audioPath, required String outputPath}) {
+    return methodChannel.invokeMethod("mergeAudioAndVideo", {
+      'input_path': inputPath,
+      'audio_path': audioPath,
+      'output_path': outputPath
+    });
+  }
+
+  Future<bool?> extractAudio(
+      {required String inputPath, required String outputPath}) {
+    return methodChannel.invokeMethod("extractAudio", {
+      'input_path': inputPath,
+      'output_path': outputPath
+    });
+  }
+
+  Future<bool?> addWaterMarkInVideo(
+      {required String inputPath, required String thumbnailPath, required String username, required String outputPath}) {
+    return methodChannel.invokeMethod("addWaterMarkInVideo", {
+      'input_path': inputPath,
+      'thumbnail_path': thumbnailPath,
+      'username': username,
+      'output_path': outputPath
+    });
+  }
+
 }
+
+
+
+
+
+
