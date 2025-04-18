ef7e4b5d01d74357e2ca6bb479c9d72dc78b6963
Aniket Vaddoriya <aniketrvaddoriya2000@gmail.com>
2025-04-18 08:32:50 +0530

    Filter & Audio to Video Added iOS

--- ios/Classes/RetrytechPlugin.swift
+++ ios/Classes/RetrytechPlugin.swift
@@ -3,74 +3,151 @@ import UIKit
 import AVKit
 
 public class RetrytechPlugin: NSObject, FlutterPlugin {
+
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "retrytech_plugin", binaryMessenger: registrar.messenger())
         let instance = RetrytechPlugin()
         registrar.addMethodCallDelegate(instance, channel: channel)
-
-        // Register the camera view
-        let cameraFactory = CameraViewFactory(messenger: registrar.messenger(), channel: channel)
-        registrar.register(cameraFactory, withId: "retrytech_camera_view")
     }
-    
+
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        guard let args = call.arguments as? [String: Any] else {
+            print("Invalid arguments format.")
+            result(false)
+            return
+        }
+
         switch call.method {
         case "mergeAudioAndVideo":
-            if let command = call.arguments as? [String: Any] {
-                if let videoInputString = command["input_path"] as? String, let audioPath = command["audio_path"] as? String, let outputPath = command["output_path"] as? String {
-                                    AVEditor.shared.mergeAudioVideo(videoInput: URL(fileURLWithPath: videoInputString),
-                                                                    audioInput: URL(fileURLWithPath: audioPath),
-                                                                    outputURL: URL(fileURLWithPath: outputPath)) { status in
-                                        result(status)
-                                    }
-                }
-            } else {
-                result(false)
-                print("Argument must be a string")
-            }
-            
+            handleMergeAudioAndVideo(args: args, result: result)
+
         case "extractAudio":
-            if let command = call.arguments as? [String: Any] {
-                print(command)
-                if let videoInputString = command["input_path"] as? String, let outputPath = command["output_path"] as? String {
-                    print("Badha Param")
-                    AVEditor.shared.extractAudio(videoURL: URL(fileURLWithPath: videoInputString), outputURL: URL(fileURLWithPath: outputPath)) { status in
-                        result(status)
-                    }
-                }
-                
-            } else {
-                result(false)
-                print("Argument must be a string")
-            }
-            
+            handleExtractAudio(args: args, result: result)
+
         case "addWaterMarkInVideo":
-            if let command = call.arguments as? [String: Any] {
-                print(command)
-                if let videoInputString = command["input_path"] as? String,
-                   let outputPath = command["output_path"] as? String,
-                   let thumbnailPath = command["thumbnail_path"] as? String {
-                    print("Badha Param")
-                    AVEditor.shared.addWatermark(videoInput: URL(fileURLWithPath: videoInputString),
-                                                 imagePath: thumbnailPath,
-                                                 username: "",
-                                                 outputURL: URL(fileURLWithPath: outputPath)) { status in
-                        result(status)
-                    }
-                }
-                
-            } else {
-                result(false)
-                print("Argument must be a string")
-            }
-            
-            
+            handleAddWatermark(args: args, result: result)
+
+        case "applyFilterToImage":
+            handleApplyFilterToImage(args: args, result: result)
+
+        case "applyFilterAndAudioToVideo":
+            handleApplyFilterAndAudioToVideo(args: args, result: result)
+
+        case "createVideoFromImage":
+            handleCreateVideoFromImage(args: args, result: result)
+
         case "getPlatformVersion":
             result("iOS " + UIDevice.current.systemVersion)
+
         default:
-        print("No");
-//            result(FlutterMethodNotImplemented)
+            result(FlutterMethodNotImplemented)
+        }
+    }
+
+    private func handleMergeAudioAndVideo(args: [String: Any], result: @escaping FlutterResult) {
+        guard let videoInputString = args["input_path"] as? String,
+              let audioPath = args["audio_path"] as? String,
+              let outputPath = args["output_path"] as? String else {
+            print("Missing parameters for merging audio and video.")
+            result(false)
+            return
+        }
+
+        AVEditor.shared.mergeAudioVideo(videoInput: URL(fileURLWithPath: videoInputString),
+                                        audioInput: URL(fileURLWithPath: audioPath),
+                                        outputURL: URL(fileURLWithPath: outputPath)) { status in
+            result(status)
+        }
+    }
+
+    private func handleExtractAudio(args: [String: Any], result: @escaping FlutterResult) {
+        guard let videoInputString = args["input_path"] as? String,
+              let outputPath = args["output_path"] as? String else {
+            print("Missing parameters for extracting audio.")
+            result(false)
+            return
+        }
+
+        AVEditor.shared.extractAudio(videoURL: URL(fileURLWithPath: videoInputString),
+                                     outputURL: URL(fileURLWithPath: outputPath)) { status in
+            result(status)
+        }
+    }
+
+    private func handleAddWatermark(args: [String: Any], result: @escaping FlutterResult) {
+        guard let videoInputString = args["input_path"] as? String,
+              let outputPath = args["output_path"] as? String,
+              let thumbnailPath = args["thumbnail_path"] as? String else {
+            print("Missing parameters for adding watermark.")
+            result(false)
+            return
+        }
+
+        AVEditor.shared.addWatermark(videoInput: URL(fileURLWithPath: videoInputString),
+                                     imagePath: thumbnailPath,
+                                     username: "", // Update if needed
+                                     outputURL: URL(fileURLWithPath: outputPath)) { status in
+            result(status)
+        }
+    }
+
+
+    private func handleApplyFilterToImage(args: [String: Any], result: @escaping FlutterResult) {
+        guard let inputPath = args["input_path"] as? String,
+              let outputPath = args["output_path"] as? String,
+              let filterValues = args["filter_values"] as? [Double] else {
+            print("Missing parameters for adding watermark.")
+            result(false)
+            return
+        }
+
+        AVEditor.shared.applyFilterToImage(imagePath: inputPath,
+                                           outputURL: URL(fileURLWithPath: outputPath),
+                                           filterValues: filterValues) { status in
+            result(status)
+        }
+    }
+
+    private func handleApplyFilterAndAudioToVideo(args: [String: Any], result: @escaping FlutterResult) {
+        guard let inputPath = args["input_path"] as? String,
+              let outputPath = args["output_path"] as? String else {
+            print("Missing parameters for adding watermark. \n\(args)")
+            result(false)
+            return
+        }
+        let audioPath = args["audio_path"] as? String
+        let startAudioTimeInMS = args["audio_start_time_in_ms"] as? Double
+        let shouldAddBothMusics = args["should_add_both_musics"] as? Bool
+        let filterValues = args["filter_values"] as? [Double]
+        AVEditor.shared.applyFilterAndAudioToVideo(inputVideoURL: URL(fileURLWithPath: inputPath),
+                                                   inputAudioURL: audioPath == nil ? nil : URL(fileURLWithPath: audioPath!),
+                                                   startTime: (startAudioTimeInMS ?? 0) / 1000,
+                                                   shouldAddBothMusic: shouldAddBothMusics ?? false,
+                                                   outputURL: URL(fileURLWithPath: outputPath),
+                                                   filterValues: filterValues ?? []) { status in
+            result(status)
+        }
+    }
+
+    private func handleCreateVideoFromImage(args: [String: Any], result: @escaping FlutterResult) {
+        guard let inputPath = args["input_path"] as? String,
+              let outputPath = args["output_path"] as? String,
+              let videoTotalDurationInSec = args["video_total_duration_in_sec"] as? Double else {
+            print("Missing parameters for adding watermark. \n\(args)")
+            result(false)
+            return
+        }
+        let audioPath = args["audio_path"] as? String
+        let startAudioTimeInMS = args["audio_start_time_in_ms"] as? Double
+        let filterValues = args["filter_values"] as? [Double]
+
+        AVEditor.shared.createVideoFromImage(from: URL(fileURLWithPath: inputPath),
+                                             withAudio: audioPath == nil ? nil : URL(fileURLWithPath: audioPath!),
+                                             outputURL: URL(fileURLWithPath: outputPath),
+                                             colorChannelMixer: filterValues ?? [],
+                                             audioStartSec: (startAudioTimeInMS ?? 0) / 1000,
+                                             videoTotalDurationInSec: videoTotalDurationInSec) { status in
+            result(status)
         }
     }
 }
-
--- lib/retrytech_plugin.dart
+++ lib/retrytech_plugin.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/services.dart';
 
 class RetrytechPlugin {
+  static var shared = RetrytechPlugin();
   final methodChannel = const MethodChannel('retrytech_plugin');
-
-  Future<bool?> runTheCommand(String command) {
-    return methodChannel.invokeMethod("runFFmpegCommand", command);
-  }
 
   Future<bool?> shareToInstagram(String command) {
     return methodChannel.invokeMethod("shareToInstagram", command);
@@ -50,6 +47,38 @@ class RetrytechPlugin {
       'thumbnail_path': thumbnailPath,
       'username': username,
       'output_path': outputPath,
+    });
+  }
+
+  Future<bool?> applyFilterToImage({
+    required String inputPath,
+    required List<double> filterValues,
+    required String outputPath,
+  }) {
+    return methodChannel.invokeMethod("applyFilterToImage", {
+      'input_path': inputPath,
+      'filter_values': filterValues,
+      'output_path': outputPath
+    });
+  }
+
+  Future<bool?> createVideoFromImage({
+    required String inputPath,
+    required String outputPath,
+    required bool shouldBothMusics,
+    String? audioPath,
+    List<double> filterValues = const [],
+    double? audioStartTimeInMS,
+    double videoTotalDurationInSec,
+  }) {
+    return methodChannel.invokeMethod("createVideoFromImage", {
+      'input_path': inputPath,
+      'audio_path': audioPath,
+      'filter_values': filterValues,
+      'output_path': outputPath,
+      'should_add_both_musics': shouldBothMusics,
+      'audio_start_time_in_ms': audioStartTimeInMS,
+      'video_total_duration_in_sec': videoTotalDurationInSec,
     });
   }
 }
